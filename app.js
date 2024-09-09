a2e2f05e74960613b51d688b55ea5369d27054ae
innovatesolutionstudio <innovatesolutionstudio@gmail.com>
2024-09-09 08:54:56 -0400

    pagina listo para testear version 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 // Configura EJS como motor de plantillas
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
 // Configura la carpeta 'public' para servir archivos estáticos
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
